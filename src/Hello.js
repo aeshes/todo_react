5f3ddb6e43fb437dd4e31ad26c448d80dbc6f8ce
aeshes <mathesis@inbox.ru>
2018-05-02 20:48:48 +0300

    delete Hello.js

--- src/Hello.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Hello = (props) => {
-    return <div>Hello, {props.name}</div>
-};
-
-export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from "./Hello";
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Hello name="Alice" />, document.getElementById("hello"));
-registerServiceWorker();
