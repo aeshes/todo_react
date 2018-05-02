c3a239b2d33f66beae810d77da2fe49c41713fff
aeshes <mathesis@inbox.ru>
2018-05-02 20:29:35 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state =
+    {
+      term: "", // to store what we passing as a value to our input
+      items: [] // store every value which we passing to our todo list
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
